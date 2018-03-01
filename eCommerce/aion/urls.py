8f6f87de40b96f68a2a60629970481504d710f1f
Ralph Guansing <ralph_guansing@dlsu.edu.ph>
2018-03-01 15:51:38 +0800

    login
    tentative login template

--- eCommerce/aion/urls.py
+++ eCommerce/aion/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
     path('home/', views.HomeView.as_view(), name='home'),
+    path('login/', views.login_view, name='login'),
     ]
--- eCommerce/aion/views.py
+++ eCommerce/aion/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse, render, redirect,HttpResponseRedirect, get_object_or_404, get_list_or_404
 from django.views.generic import TemplateView
 from django.views import generic
+from aion.forms import *
+from django.contrib.auth import authenticate, login, logout, get_user_model
 from .models import *
 class HomePageView(TemplateView):
     template_name = 'home.html'
@@ -22,3 +25,16 @@ class HomeView(generic.ListView):
 #        context["itemcount"] = self.request.GET.get('paginate_by', self.paginate_by)
 #
 #        return context
+
+
+def login_view(request):
+    title = "Login"
+    form = UserLoginForm(request.POST or None)
+    
+    if form.is_valid():
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+        user= authenticate(username=username, password=password)
+        login(request,user)
+        return HttpResponseRedirect('/home/')
+    return render(request, "aion/login.html",{"form":form, "title": title})
