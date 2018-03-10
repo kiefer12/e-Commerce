42cb741e48cd6c5dad6af068df2ad0006b00fdab
Ralph Guansing <ralph_guansing@dlsu.edu.ph>
2018-03-10 17:32:00 +0800

    file indent.

--- eCommerce/aion/urls.py
+++ eCommerce/aion/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
     path('home/', views.HomeView.as_view(), name='home'),
     path('login/', views.login_view, name='login'),
- path('addproduct/', views.CreateProduct.as_view(), name='addproduct'),
+    path('addproduct/', views.CreateProduct.as_view(), name='addproduct'),
     
     path('product/<int:pk>/', views.ViewProduct.as_view(), name='viewproduct'),
     path('user/<int:pk>/', views.ViewAccount.as_view(), name='viewaccount'),
